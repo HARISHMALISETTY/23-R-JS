57db2905aa85f6329168b02e5a909df5a1b0464a
HARISHDEV <malisettyharish@gmail.com>
2024-10-22 09:45:22 +0530

    js6

--- js/js5.js
+++ js/js5.js
@@ -1,93 +1,155 @@
-// string and string methods
+// // string and string methods
 
-var str="weLcome";
+// var str="weLcome";
 
-// console.log(str[12])
+// // console.log(str[12])
 
-// console.log(str.length)
+// // console.log(str.length)
 
-// length of the welcome is 7.
+// // length of the welcome is 7.
 
-// console.log(`length of the ${str} is ${str.length}`);
+// // console.log(`length of the ${str} is ${str.length}`);
 
-// console.log(str.at(15))
-// console.log(str[-2]) // basic
-// console.log(str.charAt(-2)); //next level
-// console.log(str.at(-2))// next-next level
-// console.log(str.charCodeAt(25)) // it will accept only positive indexes and gives ASCII  values of characters.
+// // console.log(str.at(15))
+// // console.log(str[-2]) // basic
+// // console.log(str.charAt(-2)); //next level
+// // console.log(str.at(-2))// next-next level
+// // console.log(str.charCodeAt(25)) // it will accept only positive indexes and gives ASCII  values of characters.
 
-// console.log("str".toUpperCase());
+// // console.log("str".toUpperCase());
 
-// console.log("heLLooJKJKJN".toLowerCase());
+// // console.log("heLLooJKJKJN".toLowerCase());
 
-var x3="string1";
+// var x3="string1";
 
-// console.log(x3.concat(str,"hi","something"));
+// // console.log(x3.concat(str,"hi","something"));
 
-// console.log("hello".concat("world"))
+// // console.log("hello".concat("world"))
 
-var strr="harish    ";
+// var strr="harish    ";
 
-console.log(strr.length);
+// console.log(strr.length);
 
-var str1=strr.trim(); 
-// it will not modifies the existing string but it will returns the new string by applying trim.
-// trim is nothing but removing unnecessary whitespaces.
+// var str1=strr.trim(); 
+// // it will not modifies the existing string but it will returns the new string by applying trim.
+// // trim is nothing but removing unnecessary whitespaces.
 
-console.log(str1.length);
-
-
-var strr1="  123  45  ";
-
-// console.log(strr1.length,"before trimming")
-
-// var afterTrim=strr1.trim();
-
-// console.log(afterTrim.length,"after trimming");
-
-// var trimAtStart=strr1.trimEnd();
-// // console.log(trimAtStart.length,"trimming at start");
+// console.log(str1.length);
 
 
-// var ip="wheresriya";
+// var strr1="  123  45  ";
+
+// // console.log(strr1.length,"before trimming")
+
+// // var afterTrim=strr1.trim();
+
+// // console.log(afterTrim.length,"after trimming");
+
+// // var trimAtStart=strr1.trimEnd();
+// // // console.log(trimAtStart.length,"trimming at start");
 
 
-// console.log(ip.slice(-4));
+// // var ip="wheresriya";
 
 
-//join method is used to join all the elements in the array and converts it into a string.
-
-var arr=[1,2,3,4,5];
-
-console.log(arr.join(""))
+// // console.log(ip.slice(-4));
 
 
+// //join method is used to join all the elements in the array and converts it into a string.
 
-var str1="hiee lll oop";
+// var arr=[1,2,3,4,5];
 
-console.log(str1.split(" "));
-
-
-var str3="fullstack";
-
-// console.log(str3[0])
-
-// for(iterator in variable){
-//     //tasks 
-// }
+// console.log(arr.join(""))
 
 
 
-// for(x of str3){
-//     // console.log(str3[x])
-//     console.log(x)
-// }
+// var str1="hiee lll oop";
+
+// console.log(str1.split(" "));
 
 
+// var str3="fullstack";
+
+// // console.log(str3[0])
+
+// // for(iterator in variable){
+// //     //tasks 
+// // }
+
+
+
+// // for(x of str3){
+// //     // console.log(str3[x])
+// //     console.log(x)
+// // }
 
 
 
 
 
+// // topics:
+// // -------
+// // recap of array methods and join method
+// // methods in strings
+// // at,charat,charcodeat,touppercase,tolowercase,concat,trim,trimstart,trimend,slice,split
+// // small intro to for-in/for-of
+
+
+
+
+
+
+
+// var fruits=["apple","banana","orange"];
+// // o/p= "apple,banana,orange";
+
+// console.log(fruits.join("-"))
+
+// var chars=["h","e","l","l","o"];
+
+// // hello
+
+// console.log(chars.join(""))
+
+// var text="javascript is awesome";
+// //o/p: javascript.
+
+// console.log(text.slice(0,10))
+
+
+// var text1="i love learning javascript";
+
+// console.log(text1.slice(-19,-11))
+
+
+// var date="2024-10-21";
+// //o/p:["2024","10","21"];
+
+// console.log(date.split("-"))
+
+var text="i love javascript programming";
+
+// console.log(text.split(" ",2))
+
+// programming
+
+var x=text.split(" ");
+
+// console.log(x[3])
+
+var y=x[3]; // "programming"
+var z=y.split() // insert into array
+console.log(z) // print that array.
+
+var z1=[];
+z1.push(y);
+console.log(z1)
+
+var url="https://fakestoreapi.com/products"
+
+//o/p:["https","","fakestoreapi.com","products"];
+
+
+console.log(url.split("/"))
 
 
---
+++ js/js6.js
@@ -0,0 +1,70 @@
+// for-in-loop
+//for -of-loop
+var str = "javascript";
+
+// i will iterate till length of the string (10)
+
+for (i in str) {
+//   console.log(parseInt(i)+1)
+// console.log(str[i].toUpperCase())
+}
+
+var arr=[1,2,3,4,5,6,7,8,9,10];
+for(x in arr){
+    // console.log(2,'*',x,'=',(2*(parseInt(x))));
+    // console.log("js")
+    // console.log(arr[x]*2)
+
+    // console.log(`2x${arr[x]}=${arr[x]*2}`)
+}
+
+// 2x1=2
+// 2x2=4
+// 2x3=6
+
+// x=0,1,2,3(string)*-->(implicit type conversion)
+// explicit type conversion.
+
+var str1="welcome";
+
+for(i of arr){
+    // console.log(i)
+}
+
+
+var obj={"name":"23r","strength":"55","session":"js"};
+
+
+// delete obj.strength;
+obj.location="hyd"
+
+ var x=Object.keys(obj);
+console.log(x.length)
+// obj.entries()
+// console.log(Object.entries(obj))
+
+// console.log(Object.entries(obj))
+
+// console.log(obj)
+
+
+// for(j in obj){
+//     // console.log(obj[j])
+
+//     console.log(`${j} is ${obj[j]}`)
+// }
+
+// nameis 23r 
+// strength is 55 
+// session is js
+
+
+// for( j of obj){
+//     console.log(j)
+// }
+
+
+// topics:
+// --------
+// for-in,for-of for arrays,strings and objects.Object
+// few methods in objects.
