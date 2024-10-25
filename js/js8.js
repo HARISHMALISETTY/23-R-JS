cdb3c7cc084a40248c43140bc3b4a3bcbabc32b8
HARISHDEV <malisettyharish@gmail.com>
2024-10-25 10:08:30 +0530

    operators-1

---
+++ js/js8.js
@@ -0,0 +1,124 @@
+// // var light = "yellow";
+// // if (light === "red") {
+// //   console.log("stop");
+// // } else if (light === "green") {
+// //   console.log("you can go");
+// // } else if (light === "yellow") {
+// //   console.log("wait and move");
+// // } else {
+// //   console.log("invalid light");
+// // }
+
+
+// // console.log(4/2) // division operator will gives quotient as output
+
+// // console.log(6%3);// modulus division will gives reminder as output
+
+// var x=5;
+
+// // 1.pre-increment(++x)
+// // 2.post-increment(x++)
+// // 3.pre-decrement (--x)
+// // 4.post-decrement(x--)
+// // console.log(x)
+// // console.log(++x);
+// // console.log(x++);
+// // console.log(x)
+// // console.log(x++)
+
+// // console.log(x--); // 5 will prints and then decrease to 4
+// // console.log(--x); // 4 will decrease to 3 and then print
+
+// // var y=7;
+
+// // var z=y--; // y value turns from 7 to 6
+//            // z value assign with 7 and decrement will apply for y later
+
+// // console.log(z,y);
+
+// // var x1=5;
+// // var y1= --x1;
+
+// // console.log(y1,x1)
+
+
+// var x=25;
+
+// x--;
+
+// // console.log(++x)
+
+
+// // assignment operators
+
+
+// // =
+// // +=
+
+// // var z=5;
+
+// // z=z+10;
+// // z+=10
+// // console.log(z)
+
+// var x=10;
+
+// // x=x-5;
+// // x=+5;
+// // x++;
+// // x++2;
+// // x+=4
+// // console.log(x)
+
+// var x=20;
+
+
+// // x=x**3;
+
+// // x**=3
+
+// // console.log(x)
+// // x=x*10;
+// // x*=10;
+
+// // x=x%8;
+// // console.log(x)
+
+// // console.log(x)
+
+// // x=x/5;
+// // x/=5;
+// // console.log(x)
+
+// // console.log(5!=="5")
+
+// var ip1="45";
+
+// var ip2=45;
+
+// console.log(ip1>ip2)
+
+// var ip1="a";
+
+// var ip2=4;
+
+// var op=ip1=ip2;
+
+// console.log(op,ip1,ip2)
+
+
+var y="helloo";
+
+// y=y+"23r";
+
+y+="23r"
+
+console.log(y)
+
+
+
+
+
+
+
+
