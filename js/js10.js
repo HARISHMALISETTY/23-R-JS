0bc198068caf927081c9945d57ca9ee507c5874f
HARISHDEV <malisettyharish@gmail.com>
2024-10-29 10:51:20 +0530

    terinary and for loop

--- js/js10.js
+++ js/js10.js
@@ -1,92 +1,138 @@
-// switch("user1")
-// {
-//     case "client": console.log("client logged in"); break;
+// // switch("user1")
+// // {
+// //     case "client": console.log("client logged in"); break;
 
-//     case "user":console.log("user logged in"); break;
+// //     case "user":console.log("user logged in"); break;
 
-//     default :console.log("invalid loggin"); break;
+// //     default :console.log("invalid loggin"); break;
 
-//     case "admin":console.log("admin logged in"); break;
+// //     case "admin":console.log("admin logged in"); break;
 
-//     case "hr" :console.log("hr logged in"); break;
-// }
+// //     case "hr" :console.log("hr logged in"); break;
+// // }
 
-// logical operators:
-//----------------------
+// // logical operators:
+// //----------------------
 
-// 1.OR (||)
-// it will check for any one of the multiple conditions is true
+// // 1.OR (||)
+// // it will check for any one of the multiple conditions is true
 
-// if(cnd1 || cnd2 || cnd 3){
+// // if(cnd1 || cnd2 || cnd 3){
 
-//     console.log("hello")
-// }
+// //     console.log("hello")
+// // }
 
-// var user = "admin";
-// if (user == "admin" || user == "client") {
-//   console.log("authorised");
-// } else {
-//   console.log("unauthorised access");
-// }
-// var x=5;
-// if (x<1 || x>15) {
-//     console.log("number is accepted")
-// //   console.log("true statement");
-// } else {
+// // var user = "admin";
+// // if (user == "admin" || user == "client") {
+// //   console.log("authorised");
+// // } else {
+// //   console.log("unauthorised access");
+// // }
+// // var x=5;
+// // if (x<1 || x>15) {
+// //     console.log("number is accepted")
+// // //   console.log("true statement");
+// // } else {
 
-//     console.log("number is not accepted")
-// //   console.log("false statement");
-// }
+// //     console.log("number is not accepted")
+// // //   console.log("false statement");
+// // }
 
-// var username = "akshay"; // length is 6
+// // var username = "akshay"; // length is 6
 
-// if (username.length == 0 || username.length < 10) {
-//   console.log("username is valid");
-// } else {
-//   console.log("invalid username");
-// }
+// // if (username.length == 0 || username.length < 10) {
+// //   console.log("username is valid");
+// // } else {
+// //   console.log("invalid username");
+// // }
 
-// var user="harishh";
-// var pswd="harish1234";
+// // var user="harishh";
+// // var pswd="harish1234";
 
-// if(user=="harish" && pswd =="harish123"){
+// // if(user=="harish" && pswd =="harish123"){
 
-//     console.log("successfully logged in")
-// }
-// else if(user=="harish" && pswd!="harish123"){
+// //     console.log("successfully logged in")
+// // }
+// // else if(user=="harish" && pswd!="harish123"){
 
-//     console.log("incorrect password");
-// }
-// else if(user!="harish" && pswd=="harish123"){
+// //     console.log("incorrect password");
+// // }
+// // else if(user!="harish" && pswd=="harish123"){
 
-//     console.log("incorrect username")
+// //     console.log("incorrect username")
+// // }
+// // else{
+// //     console.log("invalid credentials")
+// // }
+
+// // marks >90 -- distinction
+
+// // marks>75 and marks< 90 -- first class
+
+// // marks >60 and marks < 75 -- second class
+
+// // marks >35 and marks <60 -- third class
+
+// //marks < 35 fail
+
+// var marks =95;
+// if (marks >= 90) {
+//   console.log("passed in distinction with",marks);
+// } else if (marks >= 75 && marks < 90) {
+//   console.log("first class with", marks);
+// } else if (marks >= 60 && marks < 75) {
+//   console.log("second class with", marks);
+// } else if (marks >= 35 && marks < 60) {
+//   console.log("third class with", marks);
+// } else if (marks>=0&&marks < 35) {
+//   console.log("failed with", marks);
 // }
 // else{
-//     console.log("invalid credentials")
+//     console.log("invalid marks")
 // }
 
-// marks >90 -- distinction
+// var age=19;
+// var citizenship=true;
 
-// marks>75 and marks< 90 -- first class
+// if(age>=18 && citizenship){
+//   console.log("eligible to vote")
+// }
 
-// marks >60 and marks < 75 -- second class
+// else{
+//   console.log("not eligible")
+// }
 
-// marks >35 and marks <60 -- third class
+// var age = 20;
 
-//marks < 35 fail
+// var hasInvitation = true;
 
-var marks =95;
-if (marks >= 90) {
-  console.log("passed in distinction with",marks);
-} else if (marks >= 75 && marks < 90) {
-  console.log("first class with", marks);
-} else if (marks >= 60 && marks < 75) {
-  console.log("second class with", marks);
-} else if (marks >= 35 && marks < 60) {
-  console.log("third class with", marks);
-} else if (marks>=0&&marks < 35) {
-  console.log("failed with", marks);
-}
-else{
-    console.log("invalid marks")
+// if (age >= 21 || hasInvitation) {
+//   console.log("can enter into club");
+// } else {
+//   console.log("no entry");
+// }
+
+var gpa = 3.75;
+
+var recommendation = true;
+
+var eca = !true;
+
+// if((gpa>=3.5) && (recommendation || eca)){
+// console.log("eligible for scholarship")
+// }
+
+// else{
+//   console.log("not eligible for scholarship")
+
+// }
+
+if (gpa >= 3.5) {
+  if (recommendation || eca) {
+    console.log("eligible for scholarship");
+  } else {
+    console.log("not eligible");
+  }
+} else {
+  console.log("not eligible");
 }
---
+++ js/js11.js
@@ -0,0 +1,119 @@
+// // terinary operator
+
+// //condition?true-block:false-block
+
+// // var age=18;
+// // var citizenship= false;
+
+// // (age>=18 && citizenship)?console.log("he/she can vote"):console.log("can not vote");
+
+// var isloggedin = false;
+
+// var isSignup = !true;
+
+// isSignup
+//   ? isloggedin
+//     ? console.log("home page")
+//     : console.log("login page")
+//   : console.log("create a new account");
+
+// // isloggedin?console.log("home page"):console.log("loggin page")
+
+var isMale = true;
+var age = 22;
+
+isMale
+  ? age >= 21
+    ? console.log("he is eligible for marriage")
+    : console.log("he is not eligible for marriage")
+  : age >= 18
+  ? console.log("she is elgible to marry")
+  : console.log("she is not eligible");
+
+
+
+  //for-loop
+  //while loop
+  // do-while loop
+
+
+  // for(intialisation;condition;incre/decrement){}
+
+
+//   console.log(1)
+//   console.log(2)
+//   console.log(3);
+
+
+// for(i=1;i<=10;i++){
+//     // console.log(i)
+// }
+
+// console.log(i)
+
+
+// for(i=1;i<=5;i=i+2){
+
+//     console.log("i")
+// }
+
+var str="welcome";
+
+// for(i=0;i<str.length;i++){
+
+//     // console.log(str[i])
+
+// }
+
+// console.log(str[i])
+
+
+// var n=5;
+
+// for(i=n;i>=0;i--)
+// {
+    
+
+//     //console.log(n) //5 5 5 5 5 5 
+// }
+
+// console.log(i)
+
+
+// 1 2 3 // break
+
+// 1 2 4 5 //continue
+
+
+// for(i=0;i<=10;i++){
+    
+    
+//     if(i>=4)
+//         continue;
+//         console.log(i)
+//         // break;
+
+    
+
+    
+
+   
+//}
+
+
+
+
+var str="something";
+var op="";
+
+// op = "gnihtemos";
+
+
+for(i=0;i<=str.length-1;i++)
+{
+    op+=str[i]
+    console.log(op)  
+
+}
+
+// console.log(op)
