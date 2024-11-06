cf80b6dadecf5222772310d59ab2922170c4496f
HARISHDEV <malisettyharish@gmail.com>
2024-11-06 10:01:38 +0530

    functions-1

---
+++ js/js13.js
@@ -0,0 +1,127 @@
+// sum of two numbers
+
+// after 100 lines
+
+//sum of two numbers
+
+// after 200 lines
+
+// sum of two numbers
+debugger;
+// function f_name()
+// {
+//     console.log("hello world");
+
+//     return "hii" // it will returns hii
+
+//     console.log("something")
+
+// }
+
+// console.log(f_name());
+
+// function sample(){
+//     console.log("welcome to 10000coders");
+
+//      return "at hyderabad";
+
+// }
+
+// console.log(sample());
+
+// write a function that prints sum of 2+2
+
+// function sum() {
+//   //console.log(2 + 2);
+//   return "hello";
+// } // function definition
+
+// console.log(sum()); // function invoking
+
+// function sum(){
+
+//     var a=4;
+//     var b=5;
+
+//     // return a+b;
+// }
+// console.log(sum());
+
+// function demo(a, b) {
+//   console.log(a + b,"sum");
+//   console.log(a*b,"mul");
+//   console.log(a-b,"sub");
+
+// }
+
+// demo(3, 4);
+// demo(6,6);
+// demo(7,8);
+// demo(10,20);
+
+// 1.named function without parameters
+// 2. named function with parameters
+// 3. anonymous function
+// 4.variable function or function expression
+// 5. function with default parameters.
+//6.arrow function
+//7.callback function
+//8.higher order function
+//9.pure and impure functions
+//10.IIFE(immediately invoking function expression)
+
+
+
+// var x=function demo (y,z){
+//     console.log("iam anonymous");
+//     console.log(y+z)
+// }
+
+// x(4,5);
+
+// console.log(x);
+// var x=40;
+// demo();
+// function demo(){
+//     console.log("hello")
+// }
+
+
+
+// function sample(c,a=4,b=5){
+
+//     console.log(a+b+c)
+// }
+
+// sample(10)
+
+function _23rInfo(s_name,b_name="23r",ins="10000coders"){
+
+    console.log(`${s_name} from ${b_name} in ${ins}`)
+
+}
+
+_23rInfo("akshay");
+_23rInfo("naga sai","xyz");
+_23rInfo("nikhil");
+
+
+function loa(x)
+{    
+    if(x.length%2==0){
+        console.log("even length")
+    }
+    else{
+        console.log("odd length")
+    }
+}
+
+loa([1,2,3,4,5]);
+loa([4,5,6,7])
+
+
+// tasks
+// --------
+// 1.write a function to check whether input num is even or odd 
+// 2.write a function to check whether input string is palindrome or not
+// 3.write a function to check largest num among three numbers.
