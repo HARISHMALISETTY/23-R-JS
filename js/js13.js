544b5445291f7468405c807270a5216c4715c712
HARISHDEV <malisettyharish@gmail.com>
2024-11-09 09:34:44 +0530

    functions and scopes

--- js/js13.js
+++ js/js13.js
@@ -1,127 +1,239 @@
-// sum of two numbers
+// // // sum of two numbers
 
-// after 100 lines
+// // // after 100 lines
 
-//sum of two numbers
+// // //sum of two numbers
 
-// after 200 lines
+// // // after 200 lines
 
-// sum of two numbers
-debugger;
-// function f_name()
-// {
-//     console.log("hello world");
+// // // sum of two numbers
+// // debugger;
+// // // function f_name()
+// // // {
+// // //     console.log("hello world");
 
-//     return "hii" // it will returns hii
+// // //     return "hii" // it will returns hii
 
-//     console.log("something")
+// // //     console.log("something")
 
+// // // }
+
+// // // console.log(f_name());
+
+// // // function sample(){
+// // //     console.log("welcome to 10000coders");
+
+// // //      return "at hyderabad";
+
+// // // }
+
+// // // console.log(sample());
+
+// // // write a function that prints sum of 2+2
+
+// // // function sum() {
+// // //   //console.log(2 + 2);
+// // //   return "hello";
+// // // } // function definition
+
+// // // console.log(sum()); // function invoking
+
+// // // function sum(){
+
+// // //     var a=4;
+// // //     var b=5;
+
+// // //     // return a+b;
+// // // }
+// // // console.log(sum());
+
+// // // function demo(a, b) {
+// // //   console.log(a + b,"sum");
+// // //   console.log(a*b,"mul");
+// // //   console.log(a-b,"sub");
+
+// // // }
+
+// // // demo(3, 4);
+// // // demo(6,6);
+// // // demo(7,8);
+// // // demo(10,20);
+
+// // // 1.named function without parameters
+// // // 2. named function with parameters
+// // // 3. anonymous function
+// // // 4.variable function or function expression
+// // // 5. function with default parameters.
+// // //6.arrow function
+// // //7.callback function
+// // //8.higher order function
+// // //9.pure and impure functions
+// // //10.IIFE(immediately invoking function expression)
+
+// // // var x=function demo (y,z){
+// // //     console.log("iam anonymous");
+// // //     console.log(y+z)
+// // // }
+
+// // // x(4,5);
+
+// // // console.log(x);
+// // // var x=40;
+// // // demo();
+// // // function demo(){
+// // //     console.log("hello")
+// // // }
+
+// // // function sample(c,a=4,b=5){
+
+// // //     console.log(a+b+c)
+// // // }
+
+// // // sample(10)
+
+// // function _23rInfo(s_name,b_name="23r",ins="10000coders"){
+
+// //     console.log(`${s_name} from ${b_name} in ${ins}`)
+
+// // }
+
+// // _23rInfo("akshay");
+// // _23rInfo("naga sai","xyz");
+// // _23rInfo("nikhil");
+
+// // function loa(x)
+// // {
+// //     if(x.length%2==0){
+// //         console.log("even length")
+// //     }
+// //     else{
+// //         console.log("odd length")
+// //     }
+// // }
+
+// // loa([1,2,3,4,5]);
+// // loa([4,5,6,7])
+
+// // // tasks
+// // // --------
+// // // 1.write a function to check whether input num is even or odd
+// // // 2.write a function to check whether input string is palindrome or not
+// // // 3.write a function to check largest num among three numbers.
+// // console.log(x());
+// // var x=function sample(){
+
+// //     console.log("iam sample");
+
+// //     return "helloo"
+// // }
+// // console.log(x);
+// // var x = 45;
+
+// // function demo11(){
+
+// // }
+
+// // var demo11=()=>{}
+// // named_fun();
+// // console.log(variable_function());
+// // console.log(arrow_function())
+// // variable_function();
+// // arrow_function();
+
+// // named function
+// // function named_fun() {
+// //   console.log("iam named function");
+// // }
+
+// // variable function
+
+// // var variable_function = function () {
+// //   console.log("iam a variable function");
+// //   return "vvvv"
+// // };
+
+// // arrow function
+
+// // var arrow_function = () => {
+// //   console.log("iam a arrow function");
+// //   return "aaaa"
+// // };
+
+// // console.log(x())
+// // function x(){
+// //     return "hello"
+// // };
+
+// function demo() {}
+// var x = function () {};
+// var z = () => {};
+// var zz = () => "kk";
+
+// var y = () => {
+//   return "helloo";
+// };
+// var x = () => "hii";
+
+// console.log(x());
+
+// // var xx=()=>{
+// //     var op="hello";
+// //     return op;
+// // }
+
+// function demo() {
+//   return "hello world";
 // }
 
-// console.log(f_name());
+// console.log(demo());
 
-// function sample(){
-//     console.log("welcome to 10000coders");
+// var demo1 = function () {
+//   return "hello world";
+// };
 
-//      return "at hyderabad";
+// console.log(demo1());
 
-// }
+// var demo2 = () => {
+//   return "hello world";
+// };
 
-// console.log(sample());
+// console.log(demo2());
+// var demo2_1 = () => "hello world";
 
-// write a function that prints sum of 2+2
-
-// function sum() {
-//   //console.log(2 + 2);
-//   return "hello";
-// } // function definition
-
-// console.log(sum()); // function invoking
-
-// function sum(){
-
-//     var a=4;
-//     var b=5;
-
-//     // return a+b;
-// }
-// console.log(sum());
-
-// function demo(a, b) {
-//   console.log(a + b,"sum");
-//   console.log(a*b,"mul");
-//   console.log(a-b,"sub");
-
-// }
-
-// demo(3, 4);
-// demo(6,6);
-// demo(7,8);
-// demo(10,20);
-
-// 1.named function without parameters
-// 2. named function with parameters
-// 3. anonymous function
-// 4.variable function or function expression
-// 5. function with default parameters.
-//6.arrow function
-//7.callback function
-//8.higher order function
-//9.pure and impure functions
-//10.IIFE(immediately invoking function expression)
+// console.log(demo2_1());
 
 
 
-// var x=function demo (y,z){
-//     console.log("iam anonymous");
-//     console.log(y+z)
-// }
+var a; // decleration
 
-// x(4,5);
+var a; // re-decleration
 
-// console.log(x);
-// var x=40;
-// demo();
-// function demo(){
-//     console.log("hello")
-// }
+a=45; // assigning
 
+a=55; // re-assigning
 
+let b; // decleration
 
-// function sample(c,a=4,b=5){
+// let b; // won't allow re-decleration
 
-//     console.log(a+b+c)
-// }
+b=45; // assigning
 
-// sample(10)
+b=100; // re-assign
 
-function _23rInfo(s_name,b_name="23r",ins="10000coders"){
+var x=12; // intialisation
 
-    console.log(`${s_name} from ${b_name} in ${ins}`)
+var x=15; // re-inatialisation
 
-}
+// let y=2; // intialisation
 
-_23rInfo("akshay");
-_23rInfo("naga sai","xyz");
-_23rInfo("nikhil");
+// let y=5; // won't allow re-intialisation
 
 
-function loa(x)
-{    
-    if(x.length%2==0){
-        console.log("even length")
-    }
-    else{
-        console.log("odd length")
-    }
-}
+const z=3; // it won't allow decleration alone it will expect the value while decleration only.
 
-loa([1,2,3,4,5]);
-loa([4,5,6,7])
+// z=5;
+
+console.log(z)
 
 
-// tasks
-// --------
-// 1.write a function to check whether input num is even or odd 
-// 2.write a function to check whether input string is palindrome or not
-// 3.write a function to check largest num among three numbers.
+// global and local(block and function)
---
+++ js/scopes.js
@@ -0,0 +1,43 @@
+// // scope---life of the variable throughout the code/document/file.
+
+// // global and local(block and function)
+
+
+// var a=40;
+
+// let b=50;
+
+// const c=70;
+
+// if(true){
+//     console.log(a,b,c)
+// }
+
+// for(i=0;i<=2;i++){
+//     console.log(a,b,c)
+// }
+
+// function demo(){
+//     console.log(a,b,c)
+// }
+
+// demo()
+
+
+// if(true){
+
+//     const x=5;
+//     console.log(x)
+// }
+
+// console.log(x)
+
+
+function demo(){
+
+    const x=5;
+}
+
+demo()
+
+console.log(x)
