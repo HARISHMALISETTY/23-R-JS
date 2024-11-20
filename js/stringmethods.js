f72ae99577cfeb40a653d0db10e2c132ae52b876
HARISHMALISETTY <malisettyharish@gmail.com>
2024-11-20 09:53:55 +0530

    array and string methods

---
+++ js/stringmethods.js
@@ -0,0 +1,63 @@
+// let str="javascript";
+
+// let x=str.substr(2,4);
+
+// console.log(x)
+
+
+// let x="*";
+
+// // for(i=0;i<=5;i++)
+// //     {
+
+// //     console.log(x.repeat(i))
+// // }
+// // console.log(x.repeat(1))
+// // console.log(x.repeat(2))
+// // console.log(x.repeat(3))
+// // console.log(x.repeat(4))
+// // console.log(x.repeat(5))
+
+
+// let ip="i love JAVAscript javascript ";
+
+// let op=ip.replace(/javascript/ig,"react");
+
+// console.log(op)
+
+
+// let arr=["hello js","hi JS","learn jS","thankyou Js"];
+
+// let x=arr.map((a)=>a.replace(/js/ig,"react"));
+
+// console.log(x)
+
+
+// let str="hi HTML html css";
+
+// let op=str.replaceAll(/html/ig,"js");
+
+// console.log(op)
+
+
+
+ let str="welcome javaScript";
+
+// let op=str.lastIndexOf("c");
+
+let op=str.includes("x")
+
+console.log(op)
+
+// indexof doesnot allows regular expressions.
+//we can give second argument to decide from where it should start searching.
+
+
+// let op=str.search("c",5);
+
+// console.log(op)
+
+// search method allows regular expressions
+//we can't give second argument to decide from where it should start searching.
+
+
