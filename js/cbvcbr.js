c0e597d07d0223d5e0fb542e16ec728e40b99105
HARISHMALISETTY <malisettyharish@gmail.com>
2024-11-26 08:26:47 +0530

    events

---
+++ js/cbvcbr.js
@@ -0,0 +1,29 @@
+// function demo(x) 
+// {
+//   console.log(x);
+
+//   x[1]="hii"
+// }
+// let a = [4, 5];
+// demo(a);
+
+// console.log(a)
+
+// // console.log(a)
+
+
+// function demo(){
+
+// }
+// demo();
+
+
+// (()=>{})();
+
+let ip=["23","123","a3df","dfr4","789","weer","hello"];
+
+op=["a3df","dfr4"]
+
+// implemet with and without using predefinedmethods
+
+// reduce method in array
---
+++ js/numberandmathmethods.js
@@ -0,0 +1,57 @@
+// let num="123";
+
+// let num1=123.416789;
+// // let op=num1.toFixed(4);
+
+// // let op=num1.toPrecision(2)
+
+// // let op=isNaN(num)
+
+// // console.log(op)
+
+
+// let x="a2";
+
+// console.log(isNaN(x*2));
+
+
+
+
+// // let str=num.toString();
+
+// // console.log(typeof str)
+// // console.log(num[1])
+
+// // for(i=0;i<=num;i++){
+
+// //     console.log(num)
+// // }
+
+
+// // for(i=0;i%2==0;i++){
+
+// //     console.log(i)
+// // }
+
+
+let ip=-12.8;
+
+// let op=Math.floor(ip);
+
+// let op=Math.ceil(ip)
+
+// let op=Math.abs(ip)
+
+
+// let op=Math.pow(18,2)
+
+// let op=Math.sqrt(16)
+
+
+// let op=Math.max(2,5,1,0,0.45)
+// let ip1=[2,5,1,0,0.45];
+// let op=Math.min(...ip1)
+
+// let op=Math.random()
+console.log(Math.trunc(Math.random()*9999+1000));
+
